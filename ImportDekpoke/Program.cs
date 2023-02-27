f061805c614e1dd26d54e4a591b28a37c73f0b00
Domenico Tenace <domenicotenace99@gmail.com>
2023-02-27 11:01:09 +0100

    feat: use the old visual program.cs

--- ImportDekpoke/Program.cs
+++ ImportDekpoke/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ImportDekpoke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
