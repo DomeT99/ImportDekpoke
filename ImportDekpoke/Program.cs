8e4a154455efb790fc841bfe1ecaa9d48209f5a4
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-06 16:21:31 +0100

    feat: create menu

--- ImportDekpoke/Program.cs
+++ ImportDekpoke/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int userInput = 0;
+           
+            do
+            {
+                userInput = DisplayMenu();
+
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine(1);
+                        break;
+                    case 2:
+                        Console.WriteLine(2);
+                        break;
+                    case 3:
+                        Console.WriteLine(3);
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid option");
+                        Console.WriteLine();
+                        break;
+                }
+            } while (userInput != 4);
+        }
+
+        private static int DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose an option");
+            Console.WriteLine();
+            Console.WriteLine("1. Import Pokemon");
+            Console.WriteLine("2. Import Moves");
+            Console.WriteLine("3. Import Items");
+            Console.WriteLine("4. Exit");
+
+            var result = Console.ReadLine();
+            Console.WriteLine();
+            return Convert.ToInt32(result);
         }
     }
 }
