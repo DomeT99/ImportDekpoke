b2e2850e4e5a9c41df8c09671842afc5648496bb
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-08 17:25:08 +0100

    feat: modularize code

--- ImportDekpoke/Program.cs
+++ ImportDekpoke/Program.cs
@@ -1,14 +1,17 @@
-﻿namespace ImportDekpoke
+﻿using ImportDekpoke.Utils;
+
+namespace ImportDekpoke
 {
     internal class Program
     {
         static void Main()
         {
+
             int userInput = 0;
-           
+
             do
             {
-                userInput = DisplayMenu();
+                userInput = Menu.Choose();
 
                 switch (userInput)
                 {
@@ -21,27 +24,12 @@
                     case 3:
                         Console.WriteLine(3);
                         break;
-                    default:
-                        Console.WriteLine("Choose a valid option");
-                        Console.WriteLine();
-                        break;
+                    case 4:
+                        return;
                 }
             } while (userInput != 4);
         }
 
-        private static int DisplayMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Choose an option");
-            Console.WriteLine();
-            Console.WriteLine("1. Import Pokemon");
-            Console.WriteLine("2. Import Moves");
-            Console.WriteLine("3. Import Items");
-            Console.WriteLine("4. Exit");
 
-            var result = Console.ReadLine();
-            Console.WriteLine();
-            return Convert.ToInt32(result);
-        }
     }
 }
---
+++ ImportDekpoke/Utils/Menu.cs
@@ -0,0 +1,42 @@
+﻿namespace ImportDekpoke.Utils
+{
+    internal class Menu
+    {
+        public static string[] _voices = { "1. Import Pokemon", "2. Import Moves", "3. Import Items", "4. Exit" };
+
+        public static int Choose()
+        {
+            Display();
+
+            var result = Console.ReadLine();
+            Console.WriteLine();
+
+            return Convert.ToInt32(result);
+        }
+
+        private static void Display()
+        {
+            Console.Clear();
+           
+            Styling();
+            Console.WriteLine();
+            Console.WriteLine("Choose an option");
+            Console.WriteLine();
+
+            for (int i = 0; i < _voices.Length; i++)
+            {
+                Console.WriteLine(_voices[i]);
+            }
+            
+            Console.WriteLine();
+        }
+
+        private static void Styling()
+        {
+
+            Console.Title = "Import Dekpoke";
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+}
