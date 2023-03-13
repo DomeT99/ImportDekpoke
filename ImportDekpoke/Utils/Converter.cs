374187490d40fdd324ef019c88bf42afbe2d61b1
Domenico Tenace <domenicotenace99@gmail.com>
2023-03-13 16:39:04 +0100

    feat: create method to save json in folder and fix bug to foreach

--- ImportDekpoke/Utils/Converter.cs
+++ ImportDekpoke/Utils/Converter.cs
@@ -7,30 +7,39 @@ namespace ImportDekpoke.Utils
 {
     class Converter
     {
-        public static void ToJsonPokemon(List<Result> jsonPokeApi)
+        public async static void ToJsonPokemon(List<Result> jsonPokeApi)
         {
             List<Pokemon> pokemons = new();
             RestResponse response = new();
-            Pokemon pokemon = new();
+
 
             try
             {
-                jsonPokeApi.ForEach(async (counterPokeApi) =>
+                foreach (Result? counterPokeApi in jsonPokeApi)
                 {
+
                     response = await Call.Get(counterPokeApi.Url!);
 
                     PokemonDetails? pokemonDetails = JsonConvert.DeserializeObject<PokemonDetails>(response.Content!);
 
-                    pokemon.Id = pokemonDetails!.id;
-                    pokemon.Name = pokemonDetails!.name;
-                    pokemon.Base_Experience = pokemonDetails!.base_experience;
-                    pokemon.Height = pokemonDetails!.height;
-                    pokemon.Weight = pokemonDetails!.weight;
 
-                    pokemon.Sprites = new()
+                    Pokemon pokemon = new()
                     {
-                        Front_Default = pokemonDetails.sprites.front_default,
-                        Front_Shiny = pokemonDetails.sprites.front_shiny
+                        Id = pokemonDetails!.id,
+                        Name = pokemonDetails!.name,
+                        Base_Experience = pokemonDetails!.base_experience,
+                        Height = pokemonDetails!.height,
+                        Weight = pokemonDetails!.weight,
+
+                        Sprites = new()
+                        {
+                            Front_Default = pokemonDetails.sprites.front_default,
+                            Front_Shiny = pokemonDetails.sprites.front_shiny
+                        },
+
+                        Types = new(),
+                        Stats = new(),
+                        Moves = new()
                     };
 
                     pokemonDetails.types.ForEach(counter =>
@@ -40,7 +49,7 @@ namespace ImportDekpoke.Utils
                             Name = counter.type.name
                         };
 
-                        pokemon.Types?.Add(newType);
+                        pokemon.Types.Add(newType);
                     });
                     pokemonDetails.stats?.ForEach(counter =>
                     {
@@ -50,7 +59,7 @@ namespace ImportDekpoke.Utils
                             Base_Stat = counter.base_stat
                         };
 
-                        pokemon.Stats?.Add(newStats);
+                        pokemon.Stats.Add(newStats);
                     });
                     pokemonDetails.moves?.ForEach(counter =>
                     {
@@ -59,15 +68,25 @@ namespace ImportDekpoke.Utils
                             Name = counter.move.name
                         };
 
-                        pokemon.Moves?.Add(newMove);
+                        pokemon.Moves.Add(newMove);
                     });
 
-                });
+                    pokemons.Add(pokemon);
+                }
+
+                SaveJson(pokemons, "");
             }
             catch (Exception)
             {
                 throw;
             }
+        }
+
+        private static void SaveJson<T>(List<T> jsonList, string path)
+        {
+            string json = JsonConvert.SerializeObject(jsonList.ToList());
+            File.WriteAllText(path, json);
+
         }
     }
 }
